f72bca7ba88f69667900c811ee142b4858f20829
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-04-17 17:28:32 -0400

    Convert build.sh to bash

--- build.sh
+++ build.sh
@@ -1,94 +1,90 @@
-/*! TACHYONS v4.11.2 | http://tachyons.io */
+#!/usr/bin/env bash
 
-/*
- *
- *      ________            ______
- *      ___  __/_____ _________  /______  ______________________
- *      __  /  _  __ `/  ___/_  __ \_  / / /  __ \_  __ \_  ___/
- *      _  /   / /_/ // /__ _  / / /  /_/ // /_/ /  / / /(__  )
- *      /_/    \__,_/ \___/ /_/ /_/_\__, / \____//_/ /_//____/
- *                                 /____/
- *
- *    TABLE OF CONTENTS
- *
- *    1. External Library Includes
- *       - Normalize.css | http://normalize.css.github.io
- *    2. Tachyons Modules
- *    3. Variables
- *       - Media Queries
- *       - Colors
- *    4. Debugging
- *       - Debug all
- *       - Debug children
- *
- */
+###
+###
+###     ________            ______
+###     ___  __/_____ _________  /______  ______________________
+###     __  /  _  __ `/  ___/_  __ \_  / / /  __ \_  __ \_  ___/
+###     _  /   / /_/ // /__ _  / / /  /_/ // /_/ /  / / /(__  )
+###     /_/    \__,_/ \___/ /_/ /_/_\__, / \____//_/ /_//____/
+###                                /____/
+###
+###   TABLE OF CONTENTS
+###
+###   1. External Library Includes
+###      - Normalize.css | http://normalize.css.github.io
+###   2. Tachyons Modules
+###   3. Variables
+###      - Media Queries
+###      - Colors
+###   4. Debugging
+###      - Debug all
+###      - Debug children
+###
+###
+
+rm css/*
+
+# External Library Includes
+cat src/_normalize.css             >> css/tachyons-with-css-variables.css
 
 
-/* External Library Includes */
-@import './_normalize';
+# Modules
+cat src/_box-sizing.css            >> css/tachyons-with-css-variables.css
+cat src/_aspect-ratios.css         >> css/tachyons-with-css-variables.css
+cat src/_images.css                >> css/tachyons-with-css-variables.css
+cat src/_background-size.css       >> css/tachyons-with-css-variables.css
+cat src/_background-position.css   >> css/tachyons-with-css-variables.css
+cat src/_outlines.css              >> css/tachyons-with-css-variables.css
+cat src/_borders.css               >> css/tachyons-with-css-variables.css
+cat src/_border-colors.css         >> css/tachyons-with-css-variables.css
+cat src/_border-radius.css         >> css/tachyons-with-css-variables.css
+cat src/_border-style.css          >> css/tachyons-with-css-variables.css
+cat src/_border-widths.css         >> css/tachyons-with-css-variables.css
+cat src/_box-shadow.css            >> css/tachyons-with-css-variables.css
+cat src/_code.css                  >> css/tachyons-with-css-variables.css
+cat src/_coordinates.css           >> css/tachyons-with-css-variables.css
+cat src/_clears.css                >> css/tachyons-with-css-variables.css
+cat src/_display.css               >> css/tachyons-with-css-variables.css
+cat src/_flexbox.css               >> css/tachyons-with-css-variables.css
+cat src/_floats.css                >> css/tachyons-with-css-variables.css
+cat src/_font-family.css           >> css/tachyons-with-css-variables.css
+cat src/_font-style.css            >> css/tachyons-with-css-variables.css
+cat src/_font-weight.css           >> css/tachyons-with-css-variables.css
+cat src/_forms.css                 >> css/tachyons-with-css-variables.css
+cat src/_heights.css               >> css/tachyons-with-css-variables.css
+cat src/_letter-spacing.css        >> css/tachyons-with-css-variables.css
+cat src/_line-height.css           >> css/tachyons-with-css-variables.css
+cat src/_links.css                 >> css/tachyons-with-css-variables.css
+cat src/_lists.css                 >> css/tachyons-with-css-variables.css
+cat src/_max-widths.css            >> css/tachyons-with-css-variables.css
+cat src/_widths.css                >> css/tachyons-with-css-variables.css
+cat src/_overflow.css              >> css/tachyons-with-css-variables.css
+cat src/_position.css              >> css/tachyons-with-css-variables.css
+cat src/_opacity.css               >> css/tachyons-with-css-variables.css
+cat src/_rotations.css             >> css/tachyons-with-css-variables.css
+cat src/_skins.css                 >> css/tachyons-with-css-variables.css
+cat src/_skins-pseudo.css          >> css/tachyons-with-css-variables.css
+cat src/_spacing.css               >> css/tachyons-with-css-variables.css
+cat src/_negative-margins.css      >> css/tachyons-with-css-variables.css
+cat src/_tables.css                >> css/tachyons-with-css-variables.css
+cat src/_text-decoration.css       >> css/tachyons-with-css-variables.css
+cat src/_text-align.css            >> css/tachyons-with-css-variables.css
+cat src/_text-transform.css        >> css/tachyons-with-css-variables.css
+cat src/_type-scale.css            >> css/tachyons-with-css-variables.css
+cat src/_typography.css            >> css/tachyons-with-css-variables.css
+cat src/_utilities.css             >> css/tachyons-with-css-variables.css
+cat src/_visibility.css            >> css/tachyons-with-css-variables.css
+cat src/_white-space.css           >> css/tachyons-with-css-variables.css
+cat src/_vertical-align.css        >> css/tachyons-with-css-variables.css
+cat src/_hovers.css                >> css/tachyons-with-css-variables.css
+cat src/_z-index.css               >> css/tachyons-with-css-variables.css
+cat src/_nested.css                >> css/tachyons-with-css-variables.css
+cat src/_styles.css                >> css/tachyons-with-css-variables.css
 
+# Variables
+# Importing here will allow you to override any variables in the modules
+cat src/_colors.css                >> css/tachyons-with-css-variables.css
+cat src/_media-queries.css         >> css/tachyons-with-css-variables.css
 
-/* Modules */
-@import './_box-sizing';
-@import './_aspect-ratios';
-@import './_images';
-@import './_background-size';
-@import './_background-position';
-@import './_outlines';
-@import './_borders';
-@import './_border-colors';
-@import './_border-radius';
-@import './_border-style';
-@import './_border-widths';
-@import './_box-shadow';
-@import './_code';
-@import './_coordinates';
-@import './_clears';
-@import './_display';
-@import './_flexbox';
-@import './_floats';
-@import './_font-family';
-@import './_font-style';
-@import './_font-weight';
-@import './_forms';
-@import './_heights';
-@import './_letter-spacing';
-@import './_line-height';
-@import './_links';
-@import './_lists';
-@import './_max-widths';
-@import './_widths';
-@import './_overflow';
-@import './_position';
-@import './_opacity';
-@import './_rotations';
-@import './_skins';
-@import './_skins-pseudo';
-@import './_spacing';
-@import './_negative-margins';
-@import './_tables';
-@import './_text-decoration';
-@import './_text-align';
-@import './_text-transform';
-@import './_type-scale';
-@import './_typography';
-@import './_utilities';
-@import './_visibility';
-@import './_white-space';
-@import './_vertical-align';
-@import './_hovers';
-@import './_z-index';
-@import './_nested';
-@import './_styles';
-
-/* Variables */
-/* Importing here will allow you to override any variables in the modules */
-@import './_colors';
-@import './_media-queries';
-
-/* Debugging */
-@import './_debug-children';
-@import './_debug-grid';
-
-/* Uncomment out the line below to help debug layout issues */
-/* @import './_debug'; */
+npx cssnano css/tachyons-with-css-variables.css css/tachyons-with-css-variables.min.css
